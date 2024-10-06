7c60a234fbf5bfde28a9978299a58f098958cab4
Бачок Микола < @DESKTOP-AHTUBL4>
2024-10-06 14:09:50 +0300

    Добавьте файлы проекта.

---
+++ lab3task1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // Контрольні значення змінних
+    double a = -1, b = -4, c = 6.3;
+
+    // Обчислення функції за формулою
+    double numerator = sqrt(a / (b - a));
+    double denominator = pow(b, 2) + a * b - abs(b + 2);
+    double f = numerator / denominator;
+
+    // Виведення результату
+    cout << "f: " << f << endl;
+
+    return 0;
+}
